b7d071c44c44cb7aba46531ecf450e639e267c2b
Lean <lean_rap50rhotmail.com>
2020-09-08 20:54:25 -0300

    slide-need-css-fix

--- components/slider.tsx
+++ components/slider.tsx
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import Carousel from "react-bootstrap/Carousel";
 import {Typography} from "@material-ui/core";
+import { BorderStyle } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
       root: {
         display: "flex",
       },
+      cover: {backgroundSize: "cover"},
+
+      image: { width: "inherit", backgroundSize: "cover", borderStyle: "solid"}
     })
 );
 
 const images : ImageSlider[] = [
-    {url: "https://farm3.staticflickr.com/2761/4026163453_b499731864_b.jpg" , caption: "First slide label"},
-    {url: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Nunchaku.JPG" , caption: "Second slide label"},
-    {url: "https://mlm-s2-p.mlstatic.com/734269-MLM31220746184_062019-O.jpg" , caption: "Third slide label"},
+    {url: "https://www.myticket.de/files/K%C3%BCnstler/G/Global%20Masters/Live%202020/global_masters-myticket-banner-1251x510-xs.jpg" , caption: "First slide label"},
+    {url: "https://www.gbittermannpro.com/wp-content/uploads/2019/05/photo-homev2.jpg" , caption: "Second slide label"},
+    {url: "https://www.myticket.de/files/K%C3%BCnstler/G/Global%20Masters/Live%202020/global_masters-myticket-banner-1251x510-xs.jpg" , caption: "Third slide label"},
 ]
 type ImageSlider = {
   url: string,
@@ -34,8 +38,8 @@ export default function Slider() {
       <div className={classes.root}>
         <Carousel activeIndex={index} onSelect={handleSelect}>
           {images.map((image, index) => (
-              <Carousel.Item key={index}>
-                <img src={image.url} alt={image.caption} />
+              <Carousel.Item key={index}  >
+                <img src={image.url} alt={image.caption} className={classes.image}/>
                 <Carousel.Caption>
                   <Typography>{image.caption}</Typography>
                 </Carousel.Caption>
